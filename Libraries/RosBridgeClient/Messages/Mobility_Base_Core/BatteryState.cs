61b151f5be7de6d344c9c6e0025ad5b2059e2ded
tsdavs <tsdavs@gmail.com>
2019-04-12 16:36:44 +1000

    initial commit

---
+++ Libraries/RosBridgeClient/Messages/Mobility_Base_Core/BatteryState.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Messages.Mobility_Base_Core
+{
+
+    public class BatteryState : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "mobility_base_core_msgs/BatteryState";
+        public Standard.Header header;
+
+        public float voltage;
+        public float rate;
+        public float charge;
+        public float capacity;
+
+        public sbyte percent;
+        public sbyte state;
+
+        public BatteryState()
+        {
+            header = new Standard.Header();
+            voltage = new float();
+            rate = new float();
+            charge = new float();
+            capacity = new float();
+
+            percent = new sbyte();
+            state = new sbyte();
+        }
+    }
+}
+
---
+++ Unity3D/Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/BatterySubscriber.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class BatterySubscriber : Subscriber<Messages.Mobility_Base_Core.BatteryState>
+    {
+        private float previousRealTime;
+        private bool isMessageReceived;
+
+        private float voltage;
+        private sbyte state;
+        private float charge;
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        protected override void ReceiveMessage(Messages.Mobility_Base_Core.BatteryState message)
+        {
+            voltage = message.voltage;
+            state = message.state;
+            charge = message.charge;
+
+            //isMessageReceived = true;
+        }
+
+        private void Update()
+        {
+            Debug.Log(charge);
+
+
+            //if (isMessageReceived)
+              //  ProcessMessage();
+        }
+        /*private void ProcessMessage()
+        {
+            Debug.Log(voltage.ToString());
+
+            isMessageReceived = false;
+        }*/
+    }
+}
